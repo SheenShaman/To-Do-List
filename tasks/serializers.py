70c2a9bab318afc15684776069f1ba0c705a7ae8
Danil Sheen <dan.80956@gmail.com>
2024-07-26 22:58:40 +0300

    add validators

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
+from django.core.validators import MaxLengthValidator
+
 from tasks.models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=50, validators=[MaxLengthValidator], allow_null=False)
+    completed = serializers.BooleanField(allow_null=False)
+
     class Meta:
         model = Task
         fields = ['title', 'completed', 'user']

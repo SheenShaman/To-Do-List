ced79384f09a1a905b5894db588fbf96094d213b
Danil Sheen <dan.80956@gmail.com>
2024-07-26 17:49:14 +0300

    add doc-strings Users

--- users/views.py
+++ users/views.py
@@ -6,30 +6,34 @@ from users.models import User
 
 
 class UserCreateView(generics.CreateAPIView):
-    queryset = User.objects.all()
+    """ Создание Пользователя """
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
 
 class UserListView(generics.ListAPIView):
+    """ Просмотр списка Пользователей """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
 
 class UserDetailView(generics.RetrieveAPIView):
+    """ Просмотр одного Пользователя """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
 
 class UserUpdateView(generics.UpdateAPIView):
+    """ Редактирование Пользователя """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
 
 class UserDeleteView(generics.DestroyAPIView):
+    """ Удаление Пользователя """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]

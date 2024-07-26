03ffd1f8b60947189b5f7f9746099f203717e472
Danil Sheen <dan.80956@gmail.com>
2024-07-26 22:58:26 +0300

    add pagination

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -19,6 +20,7 @@ class TaskListView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['completed']
+    pagination_class = PageNumberPagination
 
     def get_queryset(self):
         user_id = self.kwargs['pk']

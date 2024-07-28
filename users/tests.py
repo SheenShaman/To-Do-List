3904ae28953e072081d3d16a3fc307d60bb1c9e6
Danil Sheen <dan.80956@gmail.com>
2024-07-28 14:16:31 +0300

    add users tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APIClient
+
+User = get_user_model()
+
+
+class UserTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(email='test@test.com', password='12345')
+
+    def test_create(self):
+        response = self.client.post('/users/create/', {'email': 'test@mail.com', 'password': 'password'})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_login(self):
+        response = self.client.post('/users/login/', {'email': 'test@test.com', 'password': '12345'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_token_refresh(self):
+        get_token = self.client.post('/users/login/', {'email': 'test@test.com', 'password': '12345'})
+        response = self.client.post('/users/token/refresh/', {'refresh': get_token.data['refresh']})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
